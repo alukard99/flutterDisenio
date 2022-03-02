e3ddd902227ff92dc22d36150a66c3700fccb769
alukard99 <anavanlage@gmail.com>
2022-03-02 23:34:11 +0100

    first commit

---
+++ lib/src/pages/basico_page.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+class BasicoPage extends StatelessWidget {
+  final estiloTitulo = TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold);
+  final estiloSubTitulo = TextStyle(fontSize: 18.0, color: Colors.grey);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          _crearImagen(context),
+          _crearTitulo(),
+          _crearAcciones(),
+          _crearTexto(),
+        ],
+      ),
+    ));
+  }
+
+  Widget _crearImagen(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: GestureDetector(
+        onTap: () => Navigator.pushNamed(context, 'scroll'),
+        child: Image(
+          image: NetworkImage(
+              'https://images.pexels.com/photos/814499/pexels-photo-814499.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'),
+          height: 200.0,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+
+  Widget _crearTitulo() {
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('Lago con un puente', style: estiloTitulo),
+                  SizedBox(height: 7.0),
+                  Text('Un lago en Alemania', style: estiloSubTitulo),
+                ],
+              ),
+            ),
+            Icon(Icons.star, color: Colors.red, size: 30.0),
+            Text('41', style: TextStyle(fontSize: 20.0))
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _crearAcciones() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        _accion(Icons.call, 'CALL'),
+        _accion(Icons.near_me, 'ROUTE'),
+        _accion(Icons.share, 'Share'),
+      ],
+    );
+  }
+
+  Widget _accion(IconData icon, String texto) {
+    return Column(
+      children: <Widget>[
+        Icon(icon, color: Colors.blue, size: 40.0),
+        SizedBox(height: 5.0),
+        Text(
+          texto,
+          style: TextStyle(fontSize: 15.0, color: Colors.blue),
+        )
+      ],
+    );
+  }
+
+  Widget _crearTexto() {
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 40.0),
+        child: Text(
+          'El parque de azaleas y rododendros de Kromlau (en alemán, Azaleen- und Rhododendronpark Kromlau) es un parque paisajístico de 80 hectáreas ubicado en Gablenz, Sajonia, Alemania, construido durante el siglo xix,1​ en los terrenos de una antigua propiedad feudal de Friedrich Herrmann Rötschke.2​ Después de la Segunda Guerra Mundial, el parque fue nacionalizado por el gobierno y no tiene tarifa de admisión (aunque hay una tarifa nominal de estacionamiento).3​ El parque es un ejemplo de un jardín inglés y contiene muchos estanques y lagos pequeños. Es conocido por el Rakotzbrücke, un puente especialmente construido para crear un círculo cuando se refleja en el agua debajo de él.',
+          textAlign: TextAlign.justify,
+        ),
+      ),
+    );
+  }
+}
